f575043cb9738a0ce06c181833095ba5d6d29c9a
vaishnavBorade <115063068+vaishnavBorade@users.noreply.github.com>
2025-05-02 09:12:38 +0530

    [NEW] ai-career-coach setup

---
+++ components/header.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { LayoutDashboard } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+      <nav className="container mx-auto h-16 px-4 items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={200}
+            height={60}
+            className="h-12 py-1 w-auto object-contain"
+          />
+        </Link>
+        <div>
+          <SignedIn>
+            <Link href={"/dashboard"}>
+              <Button>
+                <LayoutDashboard className="h-4 w-4" />
+                <span className="hidden md:block">Industry Insights</span>
+              </Button>
+            </Link>
+          </SignedIn>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </nav>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+};
+
+export default Header;
